3e856400e451378330b676a188dae80edc6e6b1e
Dmitrii <razuhindm@gmail.com>
2022-10-17 19:47:47 +0300

    Третья домашняя задача 9 семинара

---
+++ Task68/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите число M: ");
+int firstNumAccermanFunc = Convert.ToInt32(ReadLine());
+Write("Введите число N: ");
+int secondNumAccermanFunc = Convert.ToInt32(ReadLine());
+int accremanFuncValue = AccermanFunc(firstNumAccermanFunc,secondNumAccermanFunc);
+WriteLine(accremanFuncValue);
+
+
+int AccermanFunc(int m,int n)
+{
+    if(m==0) return n+1;
+    if (n==0) return AccermanFunc(m-1,1);
+    return AccermanFunc(m-1,AccermanFunc(m,n-1));
+}
